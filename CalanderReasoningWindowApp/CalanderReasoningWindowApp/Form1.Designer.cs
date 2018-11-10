f87332fc8b0191a1f8470faa8b0b6cf59807e7bb
alishanmca-net <alishanmca.net@gmail.com>
2018-11-10 19:12:59 +0530

    Initial commit for CalanderReasoningWindowApp

---
+++ CalanderReasoningWindowApp/CalanderReasoningWindowApp/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CalanderReasoningWindowApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.btnTestLeapYear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please Enter Year";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(227, 60);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(453, 60);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(227, 132);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(75, 23);
+            this.submitBtn.TabIndex = 5;
+            this.submitBtn.Text = "Get Answer";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // btnTestLeapYear
+            // 
+            this.btnTestLeapYear.Location = new System.Drawing.Point(343, 132);
+            this.btnTestLeapYear.Name = "btnTestLeapYear";
+            this.btnTestLeapYear.Size = new System.Drawing.Size(107, 23);
+            this.btnTestLeapYear.TabIndex = 6;
+            this.btnTestLeapYear.Text = "Test Leap Year";
+            this.btnTestLeapYear.UseVisualStyleBackColor = true;
+            this.btnTestLeapYear.Click += new System.EventHandler(this.btnTestLeapYear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 450);
+            this.Controls.Add(this.btnTestLeapYear);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Button btnTestLeapYear;
+    }
+}
+
---
+++ CalanderReasoningWindowApp/CalanderReasoningWindowApp/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalanderReasoningWindowApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            string y = dateTimePicker2.Value.DayOfWeek.ToString();
+            MessageBox.Show(y);
+        }
+
+        private void btnTestLeapYear_Click(object sender, EventArgs e)
+        {
+            int y = dateTimePicker1.Value.Year;
+
+            if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+            {
+                MessageBox.Show($"{y} is a Leap year");
+            }
+            else
+            {
+                MessageBox.Show($"{y} is not a Leap Year");
+            }
+        }
+    }
+}
